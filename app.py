6c1753057e10a879331a7a13f2a752a473f06fa0
alexander contreras trujillo <a.contrerast@uniandes.edu.co>
2023-04-14 01:10:50 -0500

    servicio nginx funcional

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-from modelos import db
-from vistas import Tareas, GestionTareas
-
+from modelos import db, Tarea
+from vistas import Tareas, GestionTareas, Health
 
 DATABASE_URI = os.environ.get(
     'DATABASE_URI') or 'postgresql://admin:admin@localhost:5432/tool'
@@ -18,8 +17,12 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
+with app_context:
+    Tarea.query.delete()
+
 cors = CORS(app)
 
 api = Api(app)
 api.add_resource(Tareas, '/api/tasks')
 api.add_resource(GestionTareas, '/api/tasks/<int:id_task>')
+api.add_resource(Health, '/health')
