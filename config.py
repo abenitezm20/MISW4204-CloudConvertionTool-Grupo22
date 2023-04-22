418e1b8d2046b50a8744d33317534c6a42bd5f03
alexander contreras trujillo <a.contrerast@uniandes.edu.co>
2023-04-22 15:36:50 -0500

    ajuste del servicio para usar variables de entorno

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
 REDIS = "redis://redis"
-# REDIS = "redis://127.0.0.1:6379"
 STATIC_FOLDER = 'files/static/'
-DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://admin:admin@localhost:5432/tool'
+DATABASE_URI = os.environ.get('DATABASE_URI')
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'json'}
 REGISTER_ALLOWED_FIELDS = ['username', 'password1', 'password2', 'email']
